8cc0f2031410e47854b004044daaabe98aa08cad
Olatunde Success <olatundesuccess54@gmail.com>
2022-04-19 10:50:08 +0100

    initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,32 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const ZuriVoting = await hre.ethers.getContractFactory("ZuriVoting");
+  const zuri_voting = await ZuriVoting.deploy("0x161eeAc6848595480D369aD876748d7d6bbD74a0", "Olatunde Success");
+
+  await zuri_voting.deployed();
+
+  console.log("ZuriVoting deployed to:", zuri_voting.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
